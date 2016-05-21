c80e380e112ceecca1439dda6ff33436ae4ba83e
Titus Wormer <tituswormer@gmail.com>
2016-05-21 12:08:06 +0200

    Update `build.js` for 0.24

--- build.js
+++ build.js
@@ -4,7 +4,15 @@
 var fs = require('fs');
 var util = require('util');
 
-var EXPRESSION = /^\.\n([\s\S]*?)^\.\n([\s\S]*?)^\.$|^#{1,6} *(.*)$/gm;
+if (process.env.VERSION < 0.24) {
+  throw new Error(
+    'This version of `commonmark.json` cannot extract specs ' +
+    'lower than 0.24.\nUse an older version of `commonmark.json` ' +
+    'instead'
+  );
+}
+
+var EXPRESSION = /^`{32} example\n([\s\S]*?)\n`{32}$|^#{1,6} *(.*)$/gm;
 
 function rewrite(data) {
     var examples = [];
@@ -14,15 +22,17 @@ function rewrite(data) {
     data.replace(/\r\n?/g, '\n')
         .replace(/→/g, '\t')
         .replace(/^<!-- END TESTS -->(.|[\n])*/m, '')
-        .replace(EXPRESSION, function($0, $1, $2, $3) {
-            if ($3) {
-                section = $3;
+        .replace(EXPRESSION, function($0, $1, $2) {
+            var example;
+            if ($2) {
+                section = $2;
             } else {
                 index++;
+                example = $1.split(/\n\.(?:\n|$)/);
 
                 examples.push({
-                    'markdown': $1,
-                    'html': $2,
+                    'markdown': example[0] + '\n',
+                    'html': example[1] ? example[1] + '\n' : '',
                     'section': section
                 });
             }
